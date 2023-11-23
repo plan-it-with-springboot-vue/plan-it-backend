25dfa173f444f1b86f4c4cffb9ee52347b0f086f
munhyerin <9993hye@naver.com>
2023-11-23 17:44:20 +0900

    feat: 커뮤니티 검색, 페이지네이션 구현

--- src/main/java/com/ssafy/planit/board/mapper/BoardMapper.java
+++ src/main/java/com/ssafy/planit/board/mapper/BoardMapper.java
@@ -6,11 +6,14 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface BoardMapper {
     void writeArticle(BoardDto boardDto) throws SQLException;
-    List<BoardDto> listArticle() throws SQLException;
+    List<BoardDto> listArticle(Map<String, Object> param) throws SQLException;
+    List<BoardDto> listArticlePopularity(Map<String, Object> param) throws SQLException;
+    int getTotalArticleCount(Map<String, Object> param) throws SQLException;
     BoardDto getArticle(int boardId) throws SQLException;
     void updateHit(int boardId) throws SQLException;
     void modifyArticle(BoardDto boardDto) throws SQLException;
--- src/main/java/com/ssafy/planit/board/service/BoardService.java
+++ src/main/java/com/ssafy/planit/board/service/BoardService.java
@@ -2,12 +2,14 @@ package com.ssafy.planit.board.service;
 
 import com.ssafy.planit.board.dto.BoardCommentDto;
 import com.ssafy.planit.board.dto.BoardDto;
+import com.ssafy.planit.board.dto.BoardListDto;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BoardService {
     void writeArticle(BoardDto boardDto) throws Exception;
-    List<BoardDto> listArticle() throws Exception;
+    BoardListDto listArticle(Map<String, String> map) throws Exception;
     BoardDto getArticle(int boardId) throws Exception;
     void modifyArticle(BoardDto boardDto) throws Exception;
     void deleteArticle(int boardId) throws Exception;
